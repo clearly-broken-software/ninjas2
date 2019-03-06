2c0c37d9f294cfd3bce29871c67e7cfa91ae6aba
Rob van den Berg <rghvdberg@gmail.com>
2019-03-06 06:16:21 +0100

    add resources

---
+++ plugins/Ninjas2/Resources/fontawesome_ttf.hpp
@@ -0,0 +1,10 @@
+#ifndef FONTAWESOME_WEBFONT_TTF_HPP_INCLUDED
+#define FONTAWESOME_WEBFONT_TTF_HPP_INCLUDED
+
+namespace font_fontawesome
+{
+    extern const unsigned char* fontawesome_ttf;
+    const unsigned int fontawesome_ttf_size = 142072;
+};
+
+#endif
