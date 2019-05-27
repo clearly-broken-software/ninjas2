0dc88ad411760e3a324319ba7d93b101cdd0f149
Rob van den Berg <rghvdberg@gmail.com>
2019-05-27 17:27:29 +0200

    add onMouse() (again)

--- plugins/Ninjas2/Widgets/src/PianoKeyboard.cpp
+++ plugins/Ninjas2/Widgets/src/PianoKeyboard.cpp
@@ -5,34 +5,44 @@ START_NAMESPACE_DISTRHO
 
 PianoKeyboard::PianoKeyboard ( Window& parent, uint oct ) noexcept
 :
-NanoWidget(parent)
-      
+NanoWidget ( parent )
+
 {
- octaves = oct;
+  octaves = oct;
 }
 
-PianoKeyboard::PianoKeyboard ( NanoWidget * parent, uint oct) noexcept
-: NanoWidget(parent)
+PianoKeyboard::PianoKeyboard ( NanoWidget * parent, uint oct ) noexcept
+:
+NanoWidget ( parent )
 {
   octaves =oct;
 }
 
 void PianoKeyboard::setCallback ( Callback* callback ) noexcept
 {
- fCallback = callback;
+  fCallback = callback;
 }
 
 void PianoKeyboard::onNanoDisplay()
 {
   uint w=getWidth();
   uint h=getHeight();
-   beginPath();
+  beginPath();
 
-     fillColor ( Color ( 255,255,100, 255 ) );
-     rect ( 0, 0, w, h );
-     fill();
-     closePath();
+  fillColor ( Color ( 255,255,100, 255 ) );
+  rect ( 0, 0, w, h );
+  fill();
+  closePath();
 }
+
+bool PianoKeyboard::onMouse ( const MouseEvent & ev )
+{
+    
+  bool hover = contains(ev.pos);
+  printf ( "button = %i\n",ev.button );
+  return hover;
+}
+
 
 
 
